e687508899d89cb14a72234d4f847a124a50d418
maqsoodahmad9679 <maqsoodahmad9679@gmail.com>
2024-08-23 11:15:52 +0500

    initial commit

---
+++ SLIDER/performjs.js
@@ -0,0 +1,22 @@
+//var $ = jQuery().noConflict();
+    $(document).ready(function(){
+
+
+
+
+        $('.slider').slick({
+            infinite: false,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+
+            prevArrow:'.prevCSS',
+            nextArrow:'.nextCSS'
+
+          });
+
+
+        
+
+
+    }
+( jQuery))
